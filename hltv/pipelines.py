9324fd0b123ceba3249127807af2be372cb5eeda
Evan Hruskar <hruskar.evan@gmail.com>
2016-04-03 20:44:17 -0400

    Good CSV Output
    Now just need to work on link following.
    Deep URL Analysis will come later.

--- hltv/pipelines.py
+++ hltv/pipelines.py
@@ -4,8 +4,15 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import scrapy
+import csv
+from hltv import settings
 
 class HltvPipeline(object):
-    def process_item(self, item, spider):
-        return item
+	def __init__(self):
+		self.csvwriter = csv.writer(open('out.csv', 'wb'))
+		self.csvwriter.writerow(['MatchLink','Date','Map','Team 1','Team 1 Score','Team 2','Team 2 Score','Event','Team 1 Link', 'Team 2 Link','Event Link'])
+
+	def process_item(self, item, spider):
+		self.csvwriter.writerow([item['matchLink'][0], item['date'][0], item['mapName'][0], item['team1'][0], item['team1Score'][0], item['team2'][0], item['team2Score'][0], item['event'][0], item['team1Link'][0], item['team2Link'][0], item['eventLink'][0]])
+		return item
